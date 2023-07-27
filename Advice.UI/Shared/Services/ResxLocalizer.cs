ab22cbeafc4f2560123716fbd0179d388e2d0c19
Paul <paul.vanbladel@gmail.com>
2023-07-27 14:19:39 +0200

    initial

---
+++ Advice.UI/Shared/Services/ResxLocalizer.cs
@@ -0,0 +1,21 @@
+﻿using Advice.UI.Shared.Resources;
+using Telerik.Blazor.Services;
+
+namespace Advice.UI.Shared.Services
+{
+    public class ResxLocalizer : ITelerikStringLocalizer
+    {
+        public string this[string name]
+        {
+            get
+            {
+                return GetStringFromResource(name);
+            }
+        }
+
+        public string GetStringFromResource(string key)
+        {
+            return TelerikMessages.ResourceManager.GetString(key, TelerikMessages.Culture);
+        }
+    }
+}
